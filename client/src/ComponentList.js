419ecc92276dc5fe9f054c014d691ed97d9e9b98
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-16 20:39:33 -0500

    quick merge

--- client/src/ComponentList.js
+++ client/src/ComponentList.js
@@ -9,7 +9,45 @@ import Psus from './components/Psus.json'
 import Ram from './components/Ram.json'
 
 const ComponentsList= () => {
+return(
+    <>
+    <Container fluid sticky="top" className="nav-container-pages">
+      <NavHomepage />
+    </Container >
+    <Container className="index-wrapper">
+      
+      <Row className="align-items-center index-hero-row">
+      
+        <Col className="index-hero-txt">
+          <h2>Parts</h2>
+          <article>
+            <p>Browse our list of components and make your new design .</p>
+          </article>
+        </Col>
+        <Col className="index-hero-img">
+        </Col>
+      </Row>
+      {/* Breadcrumbs for navigation */}
+      <Breadcrumb className="show-component-breadcrumb">
+        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>Browse Parts</Breadcrumb.Item>
+      </Breadcrumb>
+      <Row>
+        { Components ?
+          <>
+           
+          </>
+          :  
+          <h3>
+            { hasError ? 'Oops,. something went wrong!' : 'Loading...' }
+          </h3>
+        }
+      </Row>
+    </Container>
+    <Footer />
 
+  </>
+)
 }
 export default ComponentsList
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,45 +1,102 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import { getPayload } from '../helpers/auth'
+import ButtonSignup from './navbar/ButtonSignup.js'
+import ButtonLogin from './navbar/ButtonLogin.js'
+import axios from 'axios'
+import { getTokenFromLocalStorage } from '../helpers/auth'
 
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
 
-  const matchupList = data?.matchups || [];
+
+const Nav = () => {
+
+  const [userInfo, setUserInfo] = useState([])
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  const userIsAuthenticated = () => {
+    const payload = getPayload()
+    if (!payload) return false 
+    const now = Math.round(Date.now() / 1000)
+    return now < payload.exp
+  }
+// fetch user information from local storage
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/profile',
+          {
+            headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+          })
+        setUserInfo(data)
+      } catch (err) {
+        console.log('err', err)
+      }
+    }
+    getData()
+  }, [])
+  console.log('userinfo', userInfo)
 
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+    <>
+      <div className="container-fluid">
+        <nav className="homepage-navbar-wrapper">
+          <div className="nav-logo-div">
+            
+          </div>
 
-export default Home;
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <div>
+          
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            
+            
+            {!userIsAuthenticated() ? 
+              <>
+                <li className="nav-item">
+                  <Link to="/register" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Sign up
+                  </Link> 
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Login
+                  </Link> 
+                </li>
+                <ButtonSignup />
+                <ButtonLogin />
+              </>
+              :
+              // profile picture
+              <Link to="/profile" >
+                <img
+                  src={userInfo.image}
+                  alt={userInfo.username}
+                  height={50}
+                  width={50}
+                  className="nav-bar-profile-image"
+                />
+                <Navbar.Brand clasName="login-register" href="/profile"> {userInfo.username}</Navbar.Brand>
+              </Link>
+            }
+          </ul>
+
+        </nav>
+      </div>
+      
+
+    </>    
+
+  )
+}
+export default Nav
