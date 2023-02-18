9a4a359d4d71d40e3e3be56f0b8d8c111a9da00a
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-18 06:33:32 -0500

    working on routes and frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,30 @@
+// import everything over from pages and fix routes below
+import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Search from './pages/Search'
 
 
 
-const App = () => {
-
+function App() {
   return (
-
-    <BrowserRouter>
-      <Switch>
-        <Route path="/profile/:id/edit">
-          <CommentsEdit />
-        </Route> 
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/profile">
-          <UserProfile />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  )
+    <div className="App">
+  <Router>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element ={<Home />} /> 
+      <Route path="/Cart" element ={<Cart />} />
+      <Route path="/Login" element ={<Login />} />
+      <Route path="/Register" element ={<Register />} />
+      <Route path="/Search" element ={<Search />} />
+</Routes>
+    <Footer />
+  </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
